3bb01d8b7991435570b01df269825edc615e2fe0
Tymon Tobolski <i@teamon.eu>
2014-05-07 20:43:38 +0200

    Setup swagger documentation

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -2,6 +2,10 @@ module API
   module V1
     class Base < Grape::API
       mount API::V1::Hussars
+
+      add_swagger_documentation base_path: "/api",
+                                api_version: 'v1',
+                                hide_documentation_path: true
     end
   end
 end
